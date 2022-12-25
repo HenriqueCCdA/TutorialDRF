7379ea8cbda5e0a067430005d1eb45438d4d6906
Henrique Andrade <henrique.ccda@gmail.com>
2022-12-25 00:48:55 -0300

    DRF Tuturial Part3

---
+++ tutorial/snippets/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+
+from tutorial.snippets.models import Snippet
+from tutorial.snippets.serializers import SnippetSerializer
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
---
+++ tutorial/urls.py
@@ -0,0 +1,16 @@
+from rest_framework import routers
+from django.contrib import admin
+from django.urls import path, include
+
+from tutorial.quickstart import views
+
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('groups', views.GroupViewSet)
+
+
+urlpatterns = [
+    # path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('tutorial.snippets.urls')),
+]
